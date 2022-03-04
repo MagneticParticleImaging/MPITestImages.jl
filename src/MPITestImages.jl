2242bffa2e6db13d617f71057aa9abad9a2161cb
Jonas Schumacher <jonas.beuke@gmx.de>
2022-03-04 09:04:13 +0100

    Initial three dot phantom for testing

--- src/MPITestImages.jl
+++ src/MPITestImages.jl
@@ -1,5 +1,27 @@
 module MPITestImages
 
-# Write your package code here.
+using Pkg.Artifacts
+
+const remotefiles = [
+  
+]
+
+export testimage
+function testimage(name::String)
+	if name == "three_dot_corner"
+		return three_dot_corner()
+	else
+		error("The given name `$name` did not match a known test image.")
+	end
+end
+
+
+function three_dot_corner()
+	image = zeros(Float64, (81, 81))
+	image[39:41, 39:41] .= 1
+	image[39:41, 54:56] .= 1
+	image[57:59, 39:41] .= 1
+	return image
+end
 
 end
