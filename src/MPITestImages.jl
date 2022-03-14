a27060f5ccd58651ff7b796a528450eefa9dd18b
Jonas Schumacher <jonas.beuke@gmx.de>
2022-03-14 09:55:20 +0100

    Make adding on-the-fly images easier

--- src/MPITestImages.jl
+++ src/MPITestImages.jl
@@ -21,24 +21,45 @@ const remotefiles = [
 	"phantom_vessel2"
 ]
 
+onTheFlyImages = Vector{Symbol}()
+
+"""
+Add a function symbol to the list of known test image generation functions.
+"""
+function addOnTheFlyImage(fun::Symbol)
+	if !(fun in onTheFlyImages)
+		if !(fun in remotefiles)
+			push!(onTheFlyImages, fun)
+		else
+			@warn "The on-the-fly image `$(string(fun))` is already linked with a remote file."
+		end
+	else
+		error("The on-the-fly image `$(string(fun))` has already been added.")
+	end
+end
+
+export testimage_gen
+"""
+Macro for annotating functions that can be used to generate test images.
+"""
+macro testimage_gen(expr::Expr)
+	addOnTheFlyImage(expr.args[1].args[1]) # Note: use dump to debug expr
+	return expr
+end
+
 export testimage
-function testimage(name::String)
-	if name in remotefiles
+function testimage(name::String, args...; kwargs...)
+	if Symbol(name) in onTheFlyImages # Prioritize on-the-fly images over file-based ones
+		f = getfield(@__MODULE__, Symbol(name))
+		return f(args...; kwargs...)
+	elseif name in remotefiles
 		rootpath = artifact"testimages"
 		return load(joinpath(rootpath, "phantoms", name*".png"))
-	elseif name == "three_dot_corner"
-		return three_dot_corner()
 	else
 		error("The given name `$name` did not match a known test image.")
 	end
 end
 
-function three_dot_corner()
-	image = zeros(Float64, (81, 81))
-	image[39:41, 39:41] .= 1
-	image[39:41, 54:56] .= 1
-	image[57:59, 39:41] .= 1
-	return image
-end
+include("OnTheFly.jl")
 
 end
