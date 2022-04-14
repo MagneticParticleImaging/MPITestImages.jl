2e8b59d446bfaf9ca74a524afd4e54016224f3f8
Frauke Niebel <frauke.niebel@gmail.com>
2022-04-14 14:33:36 +0200

    add mixed dot definition

--- src/OnTheFly.jl
+++ src/OnTheFly.jl
@@ -7,3 +7,11 @@
 	image[57:59, 39:41] .= 1
 	return image
 end
+
+@testimage_gen function mixed_dot(size::Tuple{Integer, Integer}=(81,81))
+	image = zeros(Float64, size)
+	image[39:41, 39:41] .= 1
+	image[39:41, 54:56] .= 1
+	image[57:59, 39:41] .= 1
+	return image
+end
