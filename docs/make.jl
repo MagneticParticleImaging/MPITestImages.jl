d0eb288d5a4b78349f988b31bedd0198a1155c86
Jonas Schumacher <jonas.beuke@gmx.de>
2022-04-19 18:10:37 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,13 @@
 using MPITestImages
 using Documenter
 
-DocMeta.setdocmeta!(MPISimulation, :DocTestSetup, :(using MPITestImages); recursive=true)
+DocMeta.setdocmeta!(MPITestImages, :DocTestSetup, :(using MPITestImages); recursive=true)
 
 makedocs(;
     modules=[MPITestImages],
     authors="Jonas Schumacher <jonas.schumacher@imte.fraunhofer.de> and contributors",
     repo="https://github.com/MagneticParticleImaging/MPITestImages.jl/blob/{commit}{path}#{line}",
-    sitename="FFLReco.jl",
+    sitename="MPITestImages.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://github.com/MagneticParticleImaging/MPITestImages.jl",
