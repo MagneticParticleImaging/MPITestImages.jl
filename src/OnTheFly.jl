5cbf569e8e42f045a595d9ab532b8bdca73fd0e5
Jonas Schumacher <github735@jonasschumacher.de>
2022-04-21 21:38:46 +0200

    Fix coding style

--- src/OnTheFly.jl
+++ src/OnTheFly.jl
@@ -80,40 +80,42 @@ end
   blockY = size(block,1)
   dist = 9
 
-    for i in 0:3, j in 0:3
-      offsetX = 33
-      offsetY = 35
-      image[offsetX+i*blockY+i*dist:offsetX+(i+1)*blockY+i*dist-1, offsetY+j*blockX+j*dist:offsetY+(j+1)*blockX+j*dist-1] = block
-    end
-    
-		# Reduce image on the size 160x160
-    image = image[46:end-54, 55:end-45]
+  for i in 0:3, j in 0:3
+    offsetX = 33
+    offsetY = 35
+    image[offsetX+i*blockY+i*dist:offsetX+(i+1)*blockY+i*dist-1, offsetY+j*blockX+j*dist:offsetY+(j+1)*blockX+j*dist-1] = block
+  end
+  
+  # Reduce image on the size 160x160
+  image = image[46:end-54, 55:end-45]
 
-    M = size(image,1)
-    N = size(image,2)
-    r = 82
+  M = size(image,1)
+  N = size(image,2)
+  r = 82
 
-		# Remove parts in blocks that range out of radius using the flood-fill algorithm
-    for i in 1:M, j in 1:N
-        if round(sqrt((i-78)^2+(j-80)^2)) > r
-          if image[i,j] == 1
-            flood_fill(image, (i,j))
-          end
-        end 
-    end
+  # Remove parts in blocks that range out of radius using the flood-fill algorithm
+  for i in 1:M, j in 1:N
+    if round(sqrt((i-78)^2+(j-80)^2)) > r
+      if image[i,j] == 1
+        flood_fill(image, (i,j))
+      end
+    end 
+  end
 
-		# Remove the rest...
-		image[1:5,44:105] .= 0
-    image[20:28,20:27] .= 0
-    image[20:28,20:27] .= 0
-    image[4:12,115:121] .= 0
-    image[12:19,124:130] .= 0
-    image[46:54,152:158] .= 0
-    image[145:154,38:44] .= 0
-    image[32:39,144:151] .= 0
-    image[41:51,1:8] .= 0
-    image[151:158,60:76] .= 0
-    image[5:15,30:42] .= 0
+  # TODO: Fix this
+  # Remove the rest...
+  image[1:5, 44:105] .= 0
+  image[20:28, 20:27] .= 0
+  image[20:28, 20:27] .= 0
+  image[4:12, 115:121] .= 0
+  image[12:19, 124:130] .= 0
+  image[46:54, 152:158] .= 0
+  image[145:154, 38:44] .= 0
+  image[32:39, 144:151] .= 0
+  image[41:51, 1:8] .= 0
+  image[151:158, 60:76] .= 0
+  image[5:15, 30:42] .= 0
+
 	return image
 end
 
@@ -121,20 +123,20 @@ end
 function generateBlock(swidth, mwidth, lwidth, radius, numSquares)
 	block = zeros(40,44)
 
-	# small squares
+	# Small squares
 	dist = 4
 	for i in 0:numSquares-1, j in 0:numSquares-1
 		block[1+i*swidth+i*dist:(i+1)*swidth+i*dist, 2+j*swidth+j*dist:1+(j+1)*swidth+j*dist] .=1
 	end
 
-  # medium squares
+  # Medium squares
   dist = 4
 
   for i in 0:numSquares-1, j in 0:numSquares-1
 		block[end-i*dist-(i+1)*mwidth+1:end-i*dist-i*mwidth, end-j*dist-(j+1)*mwidth:end-j*dist-j*mwidth-1] .=1
 	end
 
-	# large squares
+	# Large squares
 	distX = 3
 	distY = 4
 
@@ -142,32 +144,31 @@ function generateBlock(swidth, mwidth, lwidth, radius, numSquares)
 		block[1+i*lwidth+i*distY:(i+1)*lwidth+i*distY, end-j*distX-(j+1)*lwidth+1:end-j*distX-j*lwidth] .=1
 	end
 
-  # circles
+  # Circles
   centerX = 5
-  centerY = size(block,1)-15
-  circle(block,centerX,centerY,radius)
+  centerY = size(block, 1)-15
+  circle(block, centerX, centerY, radius)
 
   centerX = 15
   centerY = size(block,1)-15
-  circle(block,centerX,centerY,radius)
+  circle(block, centerX, centerY, radius)
 
   centerX = 5
   centerY = size(block,1)-5
-  circle(block,centerX,centerY,radius)
+  circle(block, centerX, centerY, radius)
 
   centerX = 15
   centerY = size(block,1)-5
-  circle(block,centerX,centerY,radius)
+  circle(block, centerX, centerY, radius)
 
   return block
-
 end
 
 function circle(block,centerX, centerY, radius)
   for i in -centerX:centerX
     for j in -centerX:centerX
       if sqrt(i^2+j^2) < radius
-        block[centerY+i,centerX+j] = 1
+        block[centerY+i, centerX+j] = 1
       end 
     end
   end
@@ -177,19 +178,19 @@ end
 function flood_fill(arr, (x, y))
   # check every element in the neighborhood of the element at (x, y) in arr
   for x_off in -1:1
-      for y_off in -1:1
-          # put the next part in a try-catch block so that if any index
-          # is outside the array, we move on to the next element.
-          try
-              # if the element is a 1, change it to an 8 and call flood_fill 
-              # on it so it fills it's neighbors
-              if arr[x + x_off, y + y_off] == 1
-                  arr[x + x_off, y + y_off] = 0
-                  flood_fill(arr, (x + x_off, y + y_off))
-              end
-          catch
-              continue
-          end
+    for y_off in -1:1
+      # put the next part in a try-catch block so that if any index
+      # is outside the array, we move on to the next element.
+      try
+        # if the element is a 1, change it to a 0 and call flood_fill 
+        # on it so it fills it's neighbors
+        if arr[x + x_off, y + y_off] == 1
+          arr[x + x_off, y + y_off] = 0
+          flood_fill(arr, (x + x_off, y + y_off))
+        end
+      catch
+        continue
       end
+    end
   end
 end
