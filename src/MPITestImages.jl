5fbfde6a09fb9a72590ed639b9370ffe14038a3c
Jonas Schumacher <github735@jonasschumacher.de>
2022-05-12 15:03:50 +0200

    Add documentation for testimage

--- src/MPITestImages.jl
+++ src/MPITestImages.jl
@@ -53,9 +53,12 @@ export testimage
 """
 		$(SIGNATURES)
 
-Create a test image with the given `name`.
+Retrieve a test image with the given `name` and the matching parameters.
 
-Note: The name must correspond either 
+Note: The name must correspond either to a remote file or a function 
+			name annotated by the `testimage_gen` macro. If both exist, precedence
+			is given to the function.
+
 # Examples
 ```jldoctest
 julia> image = testimage("delta_image", (8, 8), 2; sizeOfPoint=(3, 2), distanceOfPoints=(x -> 0, x -> 4), pivot=(3, 3))
